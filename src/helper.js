af1b83c269c0f3824e96aad7501d79cb00dc0927
SumimotoTakara <96.sumimoto@gmail.com>
2021-10-03 15:56:02 +0900

    helper定義

---
+++ src/helper.js
@@ -0,0 +1,13 @@
+import distance from '@turf/distance'
+import { point } from '@turf/helpers'
+
+export function sokutei(toLat, toLng) {
+		const from = point([35.6553392, 139.6949893]) //legit
+		const to = point([toLat, toLng]) //shopの経度 緯度
+
+		const options = { units: 'meters' }
+		const result = distance(from, to, options)
+		return Math.floor(result)
+	}
+
+
