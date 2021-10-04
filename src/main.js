c18b6ee6eb606fd0707e094cb54621742471f9c4
SumimotoTakara <96.sumimoto@gmail.com>
2021-10-04 19:25:30 +0900

    Firebase 導入

--- src/main.js
+++ src/main.js
@@ -3,6 +3,22 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: "lunch-sns.firebaseapp.com",
+  projectId: "lunch-sns",
+  storageBucket: "lunch-sns.appspot.com",
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
+};
+
+// Firebase 初期化
+const app = initializeApp(firebaseConfig);
+getAnalytics(app);
 
 Vue.config.productionTip = false;
 
