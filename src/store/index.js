f55ddb815bb875169aaf9d76c2a9be93848562ce
SumimotoTakara <96.sumimoto@gmail.com>
2021-10-24 16:07:06 +0900

    コメントの追記

--- src/store/index.js
+++ src/store/index.js
@@ -6,9 +6,12 @@ import 'firebase/auth'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-    isLoading: true,
-    isShowDetail: false,
+	state: {
+		// ローディング画面の表示非表示
+		isLoading: true,
+		// Detailの表示非表示
+		isShowDetail: false,
+		// Detailに渡されるデータ
     detailShopData: {
       name: 'none',
       photo: {
@@ -16,24 +19,31 @@ export default new Vuex.Store({
           l: 'none',
         },
       },
-    },
+		},
+		// ログインユーザー
     user: null,
   },
   getters: {
     isLoading: (state) => state.isLoading,
     isShowDetail: (state) => state.isShowDetail,
-    detailShopData: (state) => state.detailShopData,
-    user: (state) => {
-      const user = firebase.auth().currentUser
+		detailShopData: (state) => state.detailShopData,
+		user: (state) => {
+			// firebaseの現在ログインしているユーザーを取得
+			const user = firebase.auth().currentUser
+			// stateのユーザーと同一か念の為確認してから処理を行う
       if (state.user == user) {
         return user
-      } else {
-        console.log('tigau')
+			} else {
+				// 違ったらログアウトさせる
         this.deleteLoginUser()
         this.logout()
-				return
+        return
       }
-    },
+		},
+		// NOTE userの情報は、必要なプロパティだけ返すGettersよりそのまま返すのがよさそうなのでコメントアウト。
+    // userName: (state) => (state.user ? state.user.displayName : null),
+    // userImg: (state) => (state.user ? state.user.photoURL : null),
+    // userUid: (state) => (state.user ? state.user.uid : null),
   },
   mutations: {
     // isLoading
@@ -68,25 +78,29 @@ export default new Vuex.Store({
     },
   },
   actions: {
-    // isLoading
+    // isLoading ローディング画面の表示停止
     stopIsLoading: ({ commit }) => {
       commit('stopIsLoading')
-    },
+		},
+		// isLoading ローディング画面表示開始
     startIsLoading: ({ commit }) => {
       commit('startIsLoading')
     },
-    // isShowDetail
+    // isShowDetail Detailの表示切り替え
     toggleIsShowDetail: ({ commit }) => {
       commit('toggleIsShowDetail')
-    },
+		},
+		//  isShowDetail Detailの表示
     showIsShowDetail: ({ commit }, shop) => {
-      commit('showIsShowDetail')
-      // this.setDetailShopData(shop);
+			commit('showIsShowDetail')
+			// Detailに必要なデータもセット
       commit('setDetailShopData', shop)
-    },
+		},
+		// isShowDetail Detailの非表示
     hiddenIsShowDetail: ({ commit }) => {
       commit('hiddenIsShowDetail')
-    },
+		},
+		// detailData Detailにデータのセット
     setDetailShopData: ({ commit }, shop) => {
       commit('setDetailShopData', shop)
     },
@@ -94,34 +108,45 @@ export default new Vuex.Store({
     /*
      * auth
      */
-    // login() {
-    //   const googleAuthProvider = new firebase.auth.GoogleAuthProvider()
-    //   firebase.auth().signInWithRedirect(googleAuthProvider)
-    // },
-    // setLoginUser({ commit }, user) {
-    // 	commit('setLoginUser', user)
-    // 	console.log('setloginuser')
-    // },
-    checkIsLogin({commit}) {
+		// Firebaseでログイン（Google）
+    login() {
+      const googleAuthProvider = new firebase.auth.GoogleAuthProvider()
+      firebase.auth().signInWithRedirect(googleAuthProvider)
+		},
+		// stateにuserを登録
+    setLoginUser({ commit }, user) {
+    	commit('setLoginUser', user)
+    	console.log('setloginuser')
+		},
+		// 未ログインがどうかをチェックして適切な処理を行う
+		checkIsLogin() {
+			const that = this
       firebase.auth().onAuthStateChanged((user) => {
         if (user) {
-          // setLoginUser(user)
-          commit('setLoginUser', user)
-          console.log('setloginuser')
+          that.setLoginUser(user)
+					console.log(user)
         } else {
-          // login()
-          const googleAuthProvider = new firebase.auth.GoogleAuthProvider()
-          firebase.auth().signInWithRedirect(googleAuthProvider)
+					// that.login()
+					that.dispatch('login')
+					console.log('erroe')
         }
       })
-    },
+		},
+		// stateよりuserをリセット
     deleteLoginUser({ commit }) {
       commit('deleteLoguinUser')
     },
-
+		// Firebaseのログアウト
     logout() {
       firebase.auth().signOut()
     },
+
+    /*
+     * Shopデータ操作
+     */
+		addLunch() {
+			
+		}
   },
   modules: {},
 })
