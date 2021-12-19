0b296eecbd55823e69d34f8272e4f64a0f86b0c9
SumimotoTakara <96.sumimoto@gmail.com>
2021-12-19 18:17:59 +0900

    loadmoreを適切に制御

--- src/views/Explore.vue
+++ src/views/Explore.vue
@@ -15,9 +15,12 @@
         </button>
       </div>
     </div>
-    <a href="" v-if="pageCount > 100" @click="prevPage">prev page</a>
+    <a href="" v-if="pageCount > 100" @click="prevPage"
+      >prev page</a
+    >
     <shop-container :shop-data="shopData"></shop-container>
     <button
+    v-if="shopData.length > 99"
       @click.stop="moreLoad"
       class="el_moreBtn"
       :class="{ el_moreBtn__loading: loadMore == 'Loading' }"
@@ -69,8 +72,9 @@ export default {
         // })
         .then((res) => {
           // filter
-          let shops = this.filterPremiumShop(res.data.results.shop) //responseの形式に注意
-
+          //let shops = this.filterPremiumShop(res.data.results.shop) //responseの形式に注意
+          
+            let shops = res.data.results.shop
           //  オブジェクトの中にshopのデータがあるような形にする
           this.shopData = []
           let n = 1 //仮ID
@@ -80,8 +84,7 @@ export default {
             n++
           })
           // 100件をシャッフルしてshopDataに格納
-            this.shopData = getShuffledArr(temp)
-        
+          this.shopData = getShuffledArr(temp)
         })
         .then(() => {
           this.stopIsLoading()
@@ -92,17 +95,18 @@ export default {
     },
 
     // ディナー予算6000円以上の高級店は弾いておく。(ランチも高いとこ多いから)
-    filterPremiumShop(shops) {
-      let result = shops.filter((shop) => {
-        // 予算コード： B001 = 1000円~2000円 B002 = 2000円~3000円 B003 = 3000円~4000円
-        return (
-          shop.budget.code == 'B001' ||
-          shop.budget.code == 'B002' ||
-          shop.budget.code == 'B003'
-        )
-      })
-      return result
-    },
+    // 一旦停止中 12/19
+    // filterPremiumShop(shops) {
+    //   let result = shops.filter((shop) => {
+    //     // 予算コード： B001 = 1000円~2000円 B002 = 2000円~3000円 B003 = 3000円~4000円
+    //     return (
+    //       shop.budget.code == 'B001' ||
+    //       shop.budget.code == 'B002' ||
+    //       shop.budget.code == 'B003'
+    //     )
+    //   })
+    //   return result
+    // },
 
     // LoadMore
     moreLoad() {
